9aa2071694ab9faf28e637c410a8b9f41f4b3489
unotanj <jounotan@usm.edu.ph>
2022-06-06 00:51:04 +0800

    initial commot

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://splendid-hotteok-dd3b04.netlify.app';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://jehanie-001-site1.htempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false,
+  },
+  apis: {
+    default: {
+      url: 'http://jehanie-001-site1.htempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
